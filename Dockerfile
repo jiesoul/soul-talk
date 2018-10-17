3e2ece4f220db6ff06fea7d57123736e1e3cd410
jiesoul <jiesoul@gmail.com>
2018-10-18 01:28:10 +0800

    config

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,13 @@ MAINTAINER jiesoul <jiesoul@gmail.com>
 
 # 项目 jar 复制到容器下
 ADD target/soul-talk.jar /app/soul-talk.jar
+COPY entrypoint.sh /app/entrypoint.sh
+RUN chmod +x /app/entrypoint.sh
 
 # 端口
 EXPOSE 3000
 
 # 运行命令
 CMD ["java", "-jar", "/app/soul-talk.jar"]
-
+#ENTRYPOINT [ "/app/entrypoint.sh" ]
+#CMD [ "soul-talk" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-# 默认本地配置文件目录
-if [ -z "$CONFIG_END" ] ;then
-        CONFIG_END = /app/config.edn
-fi
-
 if [ "$1" = 'soul-talk' ] ; then
-    java ${JAVA_OPTS} -Dconf=${CONFIG_END} -jar /app/soul-talk.jar migrate
-    exec java ${JAVA_OPTS} -Dconf=${CONFIG_END} -jar /app/soul-talk.jar "$@"
+    java -jar /app/soul-talk.jar migrate
+    exec java -jar /app/soul-talk.jar "$@"
 fi
 
 exec "$@"
