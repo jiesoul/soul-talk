271220bf944b9f5f9dd27128fedfc8722f64085c
jiesoul <jiesoul@gmail.com>
2018-10-18 17:55:47 +0800

    docker swarm config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$1" = 'soul-talk' ] ; then
+if [ "$1" = 'soul-talk' ]; then
     java -jar /app/soul-talk.jar migrate
     exec java -jar /app/soul-talk.jar "$@"
 fi
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject soul-talk "0.1.1"
+(defproject soul-talk "0.1.2"
   :description "self site"
   :url "http://github.com/jiesoul/soul-talk"
 
