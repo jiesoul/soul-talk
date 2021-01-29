1d3d5b6a1af372a505d67ff42f2fbe36861470d1
jiesoul <jiesoul@gmail.com>
2021-01-29 17:57:04 +0800

    test semantic-ui

--- client-back/src/soul_talk/auth/views.cljs
+++ client-back/src/soul_talk/auth/views.cljs
@@ -2,94 +2,60 @@
   (:require [reagent.core :as r]
             [re-frame.core :refer [dispatch subscribe]]
             [soul-talk.routes :refer [navigate!]]
-            [soul-talk.common.styles :as styles]
-            [goog.object :as gobj]
-            ["@material-ui/core" :as mui]
-            ["@material-ui/icons" :as mui-icons]
+            ["semantic-ui-react" :as sui :refer [Grid Image]]
             [soul-talk.common.views :as c])
   (:import goog.History))
 
-(defn login-styles [^js/Mui.Theme theme]
-  #js {:root   #js {:height "100vh"}
-       :image  #js {:backgroundImage    "url(https://source.unsplash.com/random)"
-                    :backgroundRepeat   "no-repeat"
-                    :backgroundSize     "cover"
-                     ;:backgroundColor    (if (= "light" (-> theme .-palette .-type))
-                     ;                      (-> theme .-palette .-grey .-50)
-                     ;                      (-> theme .-palette .-grey .-900))
-                    :backgroundPosition "center"}
-       :paper  #js {:margin        (.spacing theme 8 4)
-                    :display       "flex"
-                    :flexDirection "column"
-                    :alignItems    "center"}
-       :avatar #js {:margin          (.spacing theme 1)
-                    :backgroundColor (.-secondary theme)}
-       :form   #js {:width     "100%"
-                    :marginTop (.spacing theme 1)}
-       :submit #js {:margin (.spacing theme 3 0 2)}
-       :footer #js {:padding   (.spacing theme 3 2)
-                    :marginTop "auto"}})
-
-(defn form [{:keys [classes]}]
+(defn login-page []
   (let [site-info (subscribe [:site-info])
         login-user (r/atom {:email "" :password ""})
         email (r/cursor login-user [:email])
         password (r/cursor login-user [:password])]
-    [:> mui/Grid {:container  true
-                  :component  "main"
-                  :class-name (.-root classes)}
-     [:> mui/CssBaseline]
-     [:> mui/Grid {:item       true
-                   :xs         false
-                   :sm         4
-                   :md         7
-                   :class-name (.-image classes)}]
-     [:> mui/Grid {:item      true
-                   :xs        12
-                   :sm        8
-                   :md        5
-                   :component mui/Paper
-                   :square    true}
+    [:div {:style {:background-image "https://source.unsplash.com/random"
+                   :background-repeat "no-repeat"
+                   :background-size "cover"}}
+     [:> Grid {:text-align "center"
+               :style {:height "100vh"
+                       :background-image "https://source.unsplash.com/random"
+                       :background-repeat "no-repeat"
+                       :background-size "cover"}
+               :vertical-align "middle"}
+      [:> Grid.Column {:style {:max-width "450px"}}
+       [:> sui/Header {:as         "h2"
+                       :color      "teal"
+                       :text-align "center"}
+        (str "Log in to " (:name @site-info))]
 
-      [:div {:class-name (.-paper classes)}
-       [:> mui/Avatar {:class-name (.-avatar classes)}
-        [:> mui-icons/Home]]
-       [:> mui/Typography {:component "h1"
-                           :variant   "h4"}
-        (:name @site-info)]
-       [:form {:class-name (.-form classes)}
-        [:> mui/TextField {:variant       "outlined"
-                           :margin        "normal"
-                           :required      true
-                           :full-width    true
-                           :id            "email"
-                           :name          "email"
-                           :label         "邮箱地址"
-                           :auto-complete "email"
-                           :auto-focus    true
-                           :on-change     #(reset! email (-> % .-target .-value))}]
-        [:> mui/TextField {:variant       "outlined"
-                           :margin        "normal"
-                           :required      true
-                           :full-width    true
-                           :label         "密码"
-                           :name          "password"
-                           :id            "password"
-                           :type          "password"
-                           :auto-complete "current-password"
-                           :on-change     #(reset! password (-> % .-target .-value))}]
-        [:> mui/Button {:type       "button"
-                        :full-width true
-                        :variant    "contained"
-                        :color      "primary"
-                        :class-name (.-submit classes)
-                        :on-click   #(dispatch [:login @login-user])}
-         "登录"]
-        [:> mui/Box {:mt 5}
-         [c/copyright]]]]]]))
-
-(defn login-page []
-  (styles/with-custom-styled login-styles form))
+       [:> sui/Form {:size "large"}
+        [:> sui/Segment {:stacked true}
+         [:> sui/Form.Input {:fluid         true
+                             :margin        "normal"
+                             :icon          "user"
+                             :icon-position "left"
+                             :placeholder   "请输入 Email"
+                             :required      true
+                             :id            "email"
+                             :name          "email"
+                             :auto-focus    true
+                             :on-change     #(reset! email (-> % .-target .-value))}]
+         [:> sui/Form.Input {:fluid         true
+                             :margin        "normal"
+                             :icon          "user"
+                             :icon-position "left"
+                             :placeholder   "请输入密码"
+                             :required      true
+                             :name          "password"
+                             :id            "password"
+                             :type          "password"
+                             :on-change     #(reset! password (-> % .-target .-value))}]
+         [:> sui/Button {:type     "button"
+                         :fluid    true
+                         :size     "large"
+                         :color    "teal"
+                         :on-click #(dispatch [:login @login-user])}
+          "登录"]
+         [:div {:style {:margin-top "5px"}}
+          [c/copyright]]]]]]]))
 
 
 
